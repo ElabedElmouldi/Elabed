07eec8d7fd0bf3089be90bea5bbc34dfabeb7bd2
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-02 15:41:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
+
 import os
 import asyncio
 from flask import Flask
-from telegram import Bot
+import telegram
 
 # ================= إعدادات التلغرام الخاصة بك =================
 TELEGRAM_TOKEN = '8ف439548325:AAHOBBHy7EwcX3J5neIaf6iJuSjyGJCuZ68'
 USER_ID = '5067771509'
-# ==========================================================
+# =======================================================
 
 app = Flask(__name__)
 
+# جلب البيانات من إعدادات رندر
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
-async def send_welcome_message():
+async def send_msg():
     if TOKEN and CHAT_ID:
-        bot = Bot(token=TOKEN)
-        try:
+        bot = telegram.Bot(token=TOKEN)
+        async with bot:
             await bot.send_message(chat_id=CHAT_ID, text="السلام عليكم تم تفعيل البوت بنجاح 🚀")
-            print("Message sent to Telegram!")
-        except Exception as e:
-            print(f"Error sending message: {e}")
 
 @app.route('/')
-def home():
-    # عند زيارة الرابط، سيحاول البوت إرسال الرسالة للتأكيد
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(send_welcome_message())
-    return "Bot is Active and Message Sent!"
+def index():
+    # محاولة إرسال الرسالة عند زيارة الرابط للتأكد
+    try:
+        asyncio.run(send_msg())
+        return "Sent!"
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
