101c7e14bc33b1e8c6be77bf2c62ba48edeec1e1
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-13 13:13:22 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,1 +1,10 @@
+def backtest(prices):
+    balance = 1000
 
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            balance *= 1.01
+        else:
+            balance *= 0.99
+
+    return balance
