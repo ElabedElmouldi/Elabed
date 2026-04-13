ec5607f6c8297c4d81c9a12069eefafe4ffd90f5
ElabedElmouldi <elabed.elmouldi@gmail.com>
2026-04-13 13:17:34 +0100

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,1 +1,14 @@
 
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+class AIModel:
+    def __init__(self):
+        self.model = RandomForestClassifier(n_estimators=500)
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, x):
+        prob = self.model.predict_proba([x])[0][1]
+        return prob
